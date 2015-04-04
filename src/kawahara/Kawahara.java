1b93cb05532fe6096608d8b3cd3542dd593c5c1a
rued97 <unt.rued.ream@gmail.com>
2015-04-04 22:23:54 +0900

    てすと。

---
+++ src/kawahara/Kawahara.java
@@ -0,0 +1,20 @@
+/*
+ *  Copyright © 2014 rued97 All Rights Reserved.
+ */
+
+package kawahara;
+
+/**
+ *
+ * @author rued97
+ */
+public class Kawahara {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+	
+}
